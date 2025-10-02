21e9dbbdb46917b3c87ec90e32936ea14a502d60
quynhmyt157-eng <quynhmyt157@gmail.com>
2025-10-02 22:02:36 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const playfair = Playfair_Display({
 })
 
 export const metadata: Metadata = {
-  title: "An. - Portfolio",
+  title: "Tram Nguyen Portfolio",
   description: "Brand systems designer and developer",
   generator: "v0.app",
 }
